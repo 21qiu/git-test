616c0d5c53ad32729c565db7177250428a8d71cf
qiu <qiu@163.com>
2022-10-16 11:29:48 +0800

    first commit

---
+++ src/main/java/com/atguigu/TestGit.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @auther 21qiu
+ * @crate 2022-10-16-11:16
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.printf( "hello world!");
+    }
+}
